ddb101662395f57171ad549b757ebdaf7c0912fa
Siddharth Surapaneni <siddharthsurapaneni@Siddharths-MBP.home>
2020-07-10 21:20:17 -0400

    updated colours

--- Hackathon-plot2.py
+++ Hackathon-plot2.py
@@ -7,7 +7,7 @@ df=df[df['location']!= "International"]
 df=df[df['iso_code']!=" "]
 df=df[df["date"]=="2020-07-10"]
 fig=px.choropleth(df,locations="iso_code",color="total_cases_per_million",hover_name="location",
-    color_continuous_scale=px.colors.qualitative.Prism)
+    color_continuous_scale=px.colors.sequential.deep)
 
 
 fig.show()
